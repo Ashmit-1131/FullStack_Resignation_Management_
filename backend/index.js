b517752868c41e4de86f9ac3162c4230c8152de1
Ashmit Yadav <beingashmit@gmail.com>
2024-11-27 15:57:36 +0530

    user-authentication

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const app=require("./app")
+require("dotenv").config()
+
+const PORT=process.env.PORT||8080;
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`)
+});
